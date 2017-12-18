9ad0caa57fb28d819656f639bfbf9ad2825f26c9
macrohardcool <macrohardcool@gmail.com>
2017-12-18 20:27:27 +0800

    Add files via upload
    Reverse a String
    字串反轉，不是單一字元，而是以單字為主，如 "This is a book "變成 "book a is This"

---
+++ main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char* reverseStr(char*);
+
+int main()
+{
+    char *str1 = "This a boy a good abc!";
+
+    char * secStr = reverseStr(str1);
+
+    printf("\nat the main=%s\n",secStr);
+    return 0;
+}
+
+char* reverseStr(char *strData)
+{
+    int strLenNo = strlen(strData);
+
+    int c;
+    int sigleStrLen = 0;
+
+    char *tmp1 = (char *)malloc(strLenNo * sizeof (char));
+
+    tmp1 += strLenNo;
+    *tmp1 = '\0';
+
+    while(strLenNo-- >= 0)
+    {
+        c = *strData++;
+        sigleStrLen++;
+        if(c == ' ' || strLenNo < 0)
+        {
+            --sigleStrLen;
+            int tmpCount = 2;
+            while(sigleStrLen != 0)
+            {
+                char aChar = *(strData-tmpCount);
+                *(--tmp1) = aChar;
+                ++tmpCount;
+                --sigleStrLen;
+            }
+            if(c == ' ') *(--tmp1) = ' ';
+        }
+    }
+    printf("\nafter convert=%s\n",tmp1);
+    printf("\n");
+    return tmp1;
+}
